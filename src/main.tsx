5ece88dd7bdcd9bae252b3be4b1fe4f03e14f019
pranavshar223 <itsmepranav223@gmail.com>
2025-09-19 19:28:56 +0530

    chore: final code synchronization for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,19 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
-// import './index.css';
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
+    <StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </React.StrictMode>
+    </StrictMode>
   );
 } else {
-  throw new Error("Root element with id 'root' not found");
+  console.error("Fatal Error: Root element with id 'root' not found.");
 }
