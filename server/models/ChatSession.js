33a8ea0e399ce1236f256cafc5264d8660bec92d
pranavshar223 <itsmepranav223@gmail.com>
2026-06-16 22:49:28 +0530

    fix: delete redundant ChatSession model in favor of existing UserSession model

--- server/models/ChatSession.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const chatSessionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, default: 'New Conversation' },
-  lastActivity: { type: Date, default: Date.now }
-}, { timestamps: true });
-
-module.exports = mongoose.model('ChatSession', chatSessionSchema);
